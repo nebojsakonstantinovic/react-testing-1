0a889a652083b1ab1e8c680331f7070260f610a3
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2019-01-18 17:45:42 +0100

    part 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
+
   render() {
     return (
-      <div className="App">
-        <h2>test</h2>
+      <div className="App" data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment button</button>
       </div>
     );
   }
